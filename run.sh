fcbfa1ba7c2cc626ac6c23e4602b8ce8ac9df78b
jaeho <jaeho@plrg2.kaist.ac.kr>
2023-06-20 01:47:54 +0000

    Add results

--- run.sh
+++ run.sh
@@ -5,11 +5,10 @@ cnt=$(ls ./minimized-corpus | wc -l)
 echo $cnt files
 
 # iterate over files
-for filename in ./aflplusplus/* ; do
+#for filename in ./aflplusplus/* ; do
+for filename in ./fuzz_llex/* ; do
     echo "Processing $filename"
 
-    # add the debug function override at the beginning of the file
-    sed -i '1s;^;debug = function() end\n;' "$filename"
 
     # run lua script and capture exit code
     ./lua "$filename"
